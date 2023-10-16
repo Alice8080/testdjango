b15e7fe3731ed935dda99774e208eaf98c349a16
Alice <alisapyulzyu@gmail.com>
2023-10-16 16:53:30 +0300

    Add tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class AlbumTestCase(TestCase):
+    def setUp(self):
+        artist = Artist.objects.create(name="Artist1")
+        artist.album_set.create(name="Album1", year=2022)
+        artist.album_set.create(name="Album2", year=2023)
+        album = Album.objects.get(name="Album1")
+        album.track_set.create(name="Track1")
+        album.track_set.create(name="Track2")
+        album.track_set.create(name="Track3")
+
+    def test_animals_can_speak(self):
+        artist = Artist.objects.get(name="Artist1")
+        album = Album.objects.get(name="Album1")
+        track = Track.objects.get(name="Track1")
+        self.assertEqual(artist.name, "Artist1")
+        self.assertEqual(album.artist.name, "Artist1")
+        self.assertEqual(track.album.name, "Album1")
